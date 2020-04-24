fd4d46ae11ec207eba5c2faa0babbc0db3b12e4b
Robert <robm4958@gmail.com>
2020-04-24 09:16:55 -0400

    Made a drawStone function (#4)
    * Made a drawStone function to draw a stone at an arbitrary location in the canvas with an arbitrary color
    
    * Added snapping and placing of stones. However, game state is not saved so the stone gets removed once you move your mouse. This code definitely needs to be cleaned up. but it works
    
    * Got it switching colors and storing game state in a variable in the game loop. Also reorganized some of the code for readability purposes
    
    * Push up latest reconfig and medge comments on canvas clearing
    
    * Moving inline css over to App.css file
    
    Co-authored-by: Robert Montgomery <robert.montgomery@levvel.io>

--- web/src/App.js
+++ web/src/App.js
@@ -1,61 +1,39 @@
-import React from "react"
+import React, { useEffect, useRef } from "react"
 
 import Header from "./header/Header"
+import { gameLoop } from "./gameLoop"
+import { config } from "./config"
 import "./App.css"
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
+const App = () => {
+  useEffect(() => {
+    gameLoop(canvasRef)
+  })
 
-    this.state = {
-      numCells: 19,
-      cellSize: 40,
-      padding: 10,
-      gutter: 50, // gutter around grid
-    }
+  const canvasRef = useRef(null)
 
-    // calculate total grid size
-    this.canvasWidth = this.state.numCells * this.state.cellSize + this.state.gutter
-    this.canvasHeight = this.state.numCells * this.state.cellSize + this.state.gutter
-
-    this.updateCanvas = this.updateCanvas.bind(this)
+  const state = {
+    numCells: 19,
+    cellSize: 40,
+    padding: 10,
+    gutter: 50, // gutter around grid
+    stoneRadius: 15,
   }
 
-  componentDidMount() {
-    this.updateCanvas()
-  }
+  const canvasWidth = config.numCells * config.cellSize + config.gutter
+  const canvasHeight = config.numCells * config.cellSize + config.gutter
 
-  updateCanvas() {
-    const ctx = this.refs.canvas.getContext("2d")
-    const bw = this.state.numCells * this.state.cellSize
-    const bh = this.state.numCells * this.state.cellSize
-    const p = this.state.padding
-    const cellSize = this.state.cellSize
-
-    for (var x = 0; x <= bw; x += cellSize) {
-      ctx.moveTo(0.5 + x + p, p)
-      ctx.lineTo(0.5 + x + p, bh + p)
-    }
-
-    for (var x = 0; x <= bh; x += cellSize) {
-      ctx.moveTo(p, 0.5 + x + p)
-      ctx.lineTo(bw + p, 0.5 + x + p)
-    }
-
-    ctx.strokeStyle = "black"
-    ctx.stroke()
-  }
-
-  render() {
-    return (
-      <div>
-        <Header />
-        <main>
-          <canvas ref="canvas" id="canvas" width={this.canvasWidth} height={this.canvasHeight}></canvas>
-        </main>
-      </div>
-    )
-  }
+  return (
+    <div className="main">
+      <canvas
+        style={{ alignSelf: "flex-start" }}
+        ref={canvasRef}
+        id="canvas"
+        width={canvasWidth}
+        height={canvasHeight}
+      ></canvas>
+    </div>
+  )
 }
 
 export default App
