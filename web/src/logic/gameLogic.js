276e1870f6cd4fe5e883f675dd5e858d1a2058b4
Robert Montgomery <robm4958@gmail.com>
2020-05-25 12:09:31 -0400

    Finish up chain detection, and determining the surrounding color space

--- web/src/logic/gameLogic.js
+++ web/src/logic/gameLogic.js
@@ -21,6 +21,7 @@ const flatten = (index) => numCells * index[0] + index [1]
  */
 const unflatten = (flatCoords) => 
     [Math.floor(flatCoords / numCells), flatCoords % numCells]
+
 /*
  * Tell if an index is on the board by it's cartesian coordinates
  * @param(index) {Array} the cartesian coordinates represented as an array
@@ -32,6 +33,15 @@ const isOnBoard = (index) =>
     && Math.abs(index[0]) === index[0]
     && Math.abs(index[1]) === index[1]
 
+/*
+ * Given a flattened coordinate, this will return an array of flattened 
+ * coordinates representing all of the possible neighbors that the 
+ * point could have.
+ *
+ * @param(flattenedCoords) {Number} the flattened coordinate to the valid
+ * neighbors of.
+ * @return {Array} the list of flattened coordinates of valid neighbors
+ */
 const getValidNeighbors = (flattenedCoords) => { 
     const coords = unflatten(flattenedCoords);
     const possibleNeighbors = [
@@ -41,9 +51,54 @@ const getValidNeighbors = (flattenedCoords) => {
         [coords[0], coords[1] - 1]
     ];
 
+    // worth noting that we could easily cache these results in
+    // an array for quick access.
     return possibleNeighbors.filter(isOnBoard).map(flatten);
 }
 
-console.log(getValidNeighbors(0));
-console.log(getValidNeighbors(1));
-console.log(getValidNeighbors(20));
+/*
+ * Finds two things:
+ *   The longest chain of color starting from flattenedCoords and using the 
+ *   color found there.
+ *
+ *   The set of all colors reached (@reached). This is important for 
+ *   determining if we need to capture any stones. As an example, if we
+ *   determine that the only color in the frontier is the opposing teams
+ *   color then we know that we will need to capture all of the stones 
+ *   in the chain that we just found.
+ *  
+ */
+const findReached = (board, flattenedCoords) => {
+    const color = board[flattenedCoords];
+    const chain = new Set([flattenedCoords]);
+    const reached = new Set();
+    const frontier = [flattenedCoords];
+ 
+    while (frontier.length !== 0) {
+        const currentFlatCoords = frontier.pop();
+        chain.add(currentFlatCoords);
+
+        // for each neigbor of the location under inspection
+        // fetch all of it's valid neighbors. 
+        getValidNeighbors(currentFlatCoords).forEach((flatNeighbor) => {
+
+            // if the neighbor is the same color and we haven't already 
+            // visited it, add it to the frontier (or stack of neighbors to
+            // visit)
+            if (board[flatNeighbor] === color && !(chain.has(flatNeighbor))) {
+                frontier.push(flatNeighbor);
+
+            // if the neighbor is a different color, add the color to the set
+            // of reached colors.
+            } else if (board[flatNeighbor] !== color) { 
+                reached.add(flatNeighbor);
+            }
+        })
+    }
+
+    // return the chain, and the set of all colors reached.
+    return {
+        chain,
+        reached
+    }
+}
